e9b86c27a71d4f41d68a214b8917d0229bec6cec
Shibu Thannikkunnath <shibu@hotmail.co.in>
2019-02-11 17:10:43 -0500

    Initial project

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace netcore_caching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMemoryCache cache = new MemoryCache(new MemoryCacheOptions());
+
+            var result = cache.Set("key", "value");
+
+            Console.WriteLine($"cached value is {cache.Get("key")}");
+            
+            Console.Read();
+        }
+    }
+}
